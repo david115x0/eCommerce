679d8109ef1fe64b3b45059a2d81387d90462852
david115x0 <davidsilvafs29@gmail.com>
2021-11-30 17:30:09 -0300

    Inicio back

---
+++ e-commerce/DHcommerce/src/main/java/br/dh/ecommerce/DHcommerce/DHcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.dh.ecommerce.DHcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DHcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DHcommerceApplication.class, args);
+	}
+
+}
---
+++ e-commerce/DHcommerce/src/test/java/br/dh/ecommerce/DHcommerce/DHcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dh.ecommerce.DHcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DHcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
