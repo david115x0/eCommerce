9704f77e2be6e349adfe3df8837abfe80e270c23
david115x0 <davidsilvafs29@gmail.com>
2021-12-07 17:02:29 -0300

    Preenchendo o back!

--- e-commerce/DHcommerce/src/main/resources/data.sql
+++ e-commerce/DHcommerce/src/main/resources/data.sql
@@ -8,12 +8,20 @@ INSERT INTO CATEGORIAS (nome) VALUES ('desing');
 
 INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Front-End',1432.50,'Curso Front-End do zero ao profissional ','https://blog.revelo.com.br/wp-content/uploads/2020/09/front-end-1280x720.jpg',2);
 
-INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('UI/UX',15000,'Curso avançado UI/UX com todas as ferramentas incluídas, Especializada no MeTa Verso','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdDyKAp2Ou0-fiHxsLwxqKsib2xBsq0mDN3Q&usqp=CAU',3);
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('BootsTrap',5367.89,'Curso BootsTrap, neste curso você irá contemplar toda a documentação do bootsTrap, com aulas minuciosamente explicadas. Aprenda a utilizar uma das ferramentas mais utilizadas no mercado. ','https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg',2);
+
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Sass',1254.67,'Curso Sass, ferramenta que facilitará seu aprendizado em Front-End.','https://miro.medium.com/max/1400/1*FeiTcE7xAIKhNrRa-2_oiQ.png',2);
+
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('UIUX',15000,'Curso avançado UI/UX com todas as ferramentas incluídas, Especializada no MeTa Verso','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdDyKAp2Ou0-fiHxsLwxqKsib2xBsq0mDN3Q&usqp=CAU',3);
+
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Figma',1000,'Curso avançado focado na ferramenta figma, do básico ao avançado','https://cdn.domestika.org/c_fill,dpr_auto,f_auto,h_630,q_auto,w_1200/v1635856580/blog-post-open-graph-covers/000/008/682/8682-original.jpg?1635856580',3);
+
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('AdobeXD',1100,'Curso AdobeXD, aprenda a criar prototipos, wireframes para desktop e mobile','https://fixthephoto.com/blog/UserFiles/Image/333/7/3/experience-design-cc-logo.jpg',3);
 
 INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Java',2500,'Curso de java para iniciantes até o nível avançado. spring boot, Spring Data JPA e mais... ','https://img2.gratispng.com/20180805/iot/kisspng-logo-java-runtime-environment-programming-language-java-util-concurrentmodificationexception-%C3%96mer-5b6766ab2d98b8.1809687115335031471868.jpg',1);
 
-INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Lisboa',3000,'Viagem para portugal, com carro alugado ','Url imagem',3);
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('JavaScript',1200,'Curso javascript para iniciantes em programação.','https://www.cesar.org.br/wp-content/uploads/2017/07/curso-java-script-512x426.jpg',1);
 
-INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Alemanha',2500,'Viagem para a alemanha','Url imagem',3);
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('JavaScript',1800,'Curso javascript nivel avançado. necessário o domínio do nível básico.','https://miro.medium.com/max/724/1*YQgaKfVzK-YpxyT3NYqJAg.png',1);
 
-INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Praga', 5000, 'Viagem para portugal, com carro alugado ','Url imagem',3);
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('python', 5000, 'Curso python focado na automação de seriços web. Este curso comtemplará o nível avaçado, requer o conhecimento em outra linguagens.','https://me315-unicamp.github.io/aulas/imgs_aula11/python-logo.png',1);
