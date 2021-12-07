3acd504085d4de0e20ea0909dd029787bf9719f3
david115x0 <davidsilvafs29@gmail.com>
2021-12-07 10:35:15 -0300

    Incluíndo imagens no banco de dados

--- e-commerce/DHcommerce/src/main/resources/data.sql
+++ e-commerce/DHcommerce/src/main/resources/data.sql
@@ -1,16 +1,16 @@
-INSERT INTO CATEGORIAS (nome) VALUES ('América do Sul');
+INSERT INTO CATEGORIAS (nome) VALUES ('backend');
 
-INSERT INTO CATEGORIAS (nome) VALUES ('Oriente médio');
+INSERT INTO CATEGORIAS (nome) VALUES ('frontend');
 
-INSERT INTO CATEGORIAS (nome) VALUES ('Europa');
+INSERT INTO CATEGORIAS (nome) VALUES ('desing');
 
 
 
-INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Argentina',1432.50,'Viagem Argentina para duas pessoas ','Url imagem',1);
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Front-End',1432.50,'Curso Front-End do zero ao profissional ','https://blog.revelo.com.br/wp-content/uploads/2020/09/front-end-1280x720.jpg',2);
 
-INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('irã',10000,'Viagem para o irã sem volta','url imagem',2);
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('UI/UX',15000,'Curso avançado UI/UX com todas as ferramentas incluídas, Especializada no MeTa Verso','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdDyKAp2Ou0-fiHxsLwxqKsib2xBsq0mDN3Q&usqp=CAU',3);
 
-INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Páris',4000,'Viagem para portugal, com carro alugado ','Url imagem',3);
+INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Java',2500,'Curso de java para iniciantes até o nível avançado. spring boot, Spring Data JPA e mais... ','https://img2.gratispng.com/20180805/iot/kisspng-logo-java-runtime-environment-programming-language-java-util-concurrentmodificationexception-%C3%96mer-5b6766ab2d98b8.1809687115335031471868.jpg',1);
 
 INSERT INTO PRODUTOS (titulo, preco, descricao, imagem, categoria_id) VALUES ('Lisboa',3000,'Viagem para portugal, com carro alugado ','Url imagem',3);
 
